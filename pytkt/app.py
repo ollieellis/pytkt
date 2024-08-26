d7f61f3f63e915b7d69d615d29b83bc959ae8e84
ollieellis <ollieellis1@outlook.com>
2024-08-26 18:57:48 +0100

    bare functions working

--- pytkt/app.py
+++ pytkt/app.py
@@ -30,6 +30,41 @@ class CmdLineApp:
     def __init__(self) -> None:
         self.lists = {}
 
+    def dispacth(self, argsv):
+        if len(argsv) == 1:
+            self.display()
+            return
+        
+        match argsv[1].lower():
+            case "move":
+                self.move_func(argsv[2:])
+            case "add":
+                self.add_func(argsv[2:])
+            case "remove":
+                self.remove_func(argsv[2:])
+
+        self.display()
+        try:
+            self.store_all()
+        except Exception as e:
+            logger.error(f"failed to store: {e}")
+
+    def add_func(self, remaining_args):
+        params = self.unpack_add_params(remaining_args)
+        if params is None:
+            return 
+        self.add(*params)
+
+    def move_func(self, remaining_args):
+        params = self.unpack_move_params(remaining_args)
+        if params is None:
+            return 
+        self.move(*params)
+
+    def remove_func(self, remaining_args):
+        ticket_id = self.unpack_remove_args(remaining_args)
+        self.remove(ticket_id)
+
     def add(self, task: str, dest_list: str, minutes: int=None, parent_id=None, other=None):
         if dest_list not in self.lists:
             if input(f"No list: {dest_list}, Do you want to create new list?: Y/N").upper() not in ["Y", "YES"]:
@@ -38,7 +73,6 @@ class CmdLineApp:
         id_ = self.get_next_index(self.lists)
         ticket = Ticket(id_, task, minutes, parent_id, other)
         self.lists[dest_list].append(ticket)
-        self.display()
 
     def move(self, ticket_id: int, dest_list: str):
         try:
@@ -50,7 +84,6 @@ class CmdLineApp:
             self.lists[ticket_index[0]].pop(ticket_index[1])
         except Exception as e:
             logger.error(f"Failed to move ticket {e}")
-        self.display()
 
     def remove(self, ticket_id: int):
         ticket_index = self.get_ticket_index(ticket_id)
@@ -58,7 +91,6 @@ class CmdLineApp:
             print("No ticket Found")
             return
         self.lists[ticket_index[0]].pop(ticket_index[1])
-        self.display()
 
     def store_all(self, filename: str = "/Users/ollieellis/prototypes/pytkt/tickets.json"):
         json_dict = self.list_to_dict(self.lists)
@@ -85,7 +117,29 @@ class CmdLineApp:
             print(lname)
             for ticket in ticket_list:
                 print(f"\t {ticket.id_}: {ticket.task}")
-            print("-"*20)
+            print("-"*60)
+
+    def unpack_add_params(self, remaining_args):
+        if len(remaining_args) < 2:
+            print("Add requires Task def: str and dest_list: str as minumum")
+            return
+        params = remaining_args + [None]*max(0, 5-len(remaining_args))
+        return params[:5]
+    
+    def unpack_move_params(self, remaining_args):
+        if len(remaining_args) != 2:
+            print("Add requires task_id: int and dest_list: str as minumum")
+            return 
+        remaining_args[0] = int(remaining_args[0])
+        return remaining_args
+
+    def unpack_remove_args(self, remaining_args):
+        if len(remaining_args) != 1:
+            print("Add requires task_id: int")
+            return 
+        remaining_args[0] = int(remaining_args[0])
+        return remaining_args[0]
+        
 
     def list_to_dict(self, lists: Dict[str, List[Ticket]]):
         return {k: [asdict(ticket) for ticket in v] for k,v in lists.items()}
@@ -104,4 +158,4 @@ class CmdLineApp:
         for lname, ticket_list in lists.items():
             for i, ticket in enumerate(ticket_list):
                 max_id = max(max_id, ticket.id_)
-        return max_id
+        return max_id + 1
